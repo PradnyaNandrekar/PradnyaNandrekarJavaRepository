fcc87cb83e356c06196e0a2fc83a254778b8d829
PradnyaNandrekar <nandrekarpradnyas@gmail.com>
2024-10-27 10:56:08 +0530

    TestNG Assignment

---
+++ TestNG_Assignments_Pradnya_Nandrekar/Assignment64_Assertion_Amazon_Search.java
@@ -0,0 +1,30 @@
+package Assignment_TestNG_Pradnya;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Assignment64_Assertion_Amazon_Search 
+{
+	@Test
+	public void amazonSearch()
+	{
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.amezon.in");
+		WebElement txtBox_search = driver.findElement(By.id("twotabsearchtextbox"));
+		txtBox_search.sendKeys("Shoes"+Keys.ENTER);
+		
+		List<WebElement> l1 = driver.findElements(By.xpath("//span[@class='rush-component']/a/div"));
+		System.out.println(l1.size());
+		
+		Assert.assertEquals((l1.size()>1), true, "Product search test case failed");
+		
+	}
+}
---
+++ TestNG_Assignments_Pradnya_Nandrekar/Assignment70_Grouping_TestCases.java
@@ -0,0 +1,32 @@
+package Assignment_TestNG_Pradnya;
+
+import org.testng.annotations.Test;
+
+public class Assignment70_Grouping_TestCases 
+{
+	@Test(groups= {"System"})
+	public void TC1()
+	{
+		System.out.println("TC1-System");
+	}
+	@Test(groups= {"System", "Smoke"})
+	public void TC2()
+	{
+		System.out.println("TC2-System, Smoke");
+	}
+	@Test(groups= {"Integration"})
+	void TC3()
+	{
+		System.out.println("TC3-Integration");
+	}
+	@Test(groups= {"Smoke"})
+	public void TC4()
+	{
+		System.out.println("TC4-Smoke");
+	}
+	@Test(groups= {"Group5"})
+	public void TC5()
+	{
+		System.out.println("TC5-Group5");
+	}
+}
